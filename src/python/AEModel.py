471902c873881bb8a27e5c216dcc3f019e1b563e
bdoerfchen <git@bissendorf.co>
2024-02-06 19:28:11 +0100

    Methods in AEModel now with self

--- src/python/AEModel.py
+++ src/python/AEModel.py
@@ -1,18 +1,18 @@
 class AEModel:
     """Base class for autoencoder models to inherit from. Will be used to encode and decode images"""
 
-    def load() -> None:
+    def load(self) -> None:
         """Initial method to load the model from disk and initialize"""
         pass
 
-    def getName() -> str:
+    def getName(self) -> str:
         """Return the name of the model"""
         return ""
     
-    def encode(img) -> list[float]:
+    def encode(self, img) -> list[float]:
         """Encode an image and return the latent encoding vector"""
         return []
     
-    def decode(latent: list[float]) -> []:
+    def decode(self, latent: list[float]) -> []:
         """Decode the latent vector into an image"""
         return []
